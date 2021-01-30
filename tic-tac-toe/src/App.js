27d4ce42b9800126d329d24436e493f28067e823
Rasmus Falk <1998.rasmus.falk@gmail.com>
2021-01-30 19:10:26 +0100

    Add board and players

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+import './App.scss';
+
+const PLAYERS = {
+  PLAYER_1: 'Player 1',
+  PLAYER_2: 'Player 2',
+};
 
 function App() {
+  const [board, setBoard] = useState([
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ]);
+
+  const [turn, setTurn] = useState(PLAYERS.PLAYER_1);
+
+  const handleClick = (y, x) => {
+    if (turn === PLAYERS.PLAYER_1) {
+      board[y][x] = 'X';
+    } else {
+      board[y][x] = 'O';
+    }
+
+    setBoard(board);
+    setTurn(turn === PLAYERS.PLAYER_1 ? PLAYERS.PLAYER_2 : PLAYERS.PLAYER_1);
+
+    console.log(board, turn);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Player {turn}'s turn</h1>
+
+      <div className="Board">
+        {board.map((row, i) => (
+          <div className="Board__row">
+            {row.map((square, j) => (
+              <div
+                className="Board__row__square"
+                onClick={() => handleClick(i, j)}
+              >
+                {board[i][j]}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
