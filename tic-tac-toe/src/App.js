e25ddfc283632bae81f71d5c517c383b62cfdf3e
Rasmus Falk <1998.rasmus.falk@gmail.com>
2021-01-31 18:52:51 +0100

    Detect winner and tie

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -8,43 +8,74 @@ const PLAYERS = {
 };
 
 function App() {
-  const [board, setBoard] = useState([
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-  ]);
-
+  const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(PLAYERS.PLAYER_1);
+  const [winner, setWinner] = useState(null);
+  const [tie, setTie] = useState(null);
 
-  const handleClick = (y, x) => {
-    if (turn === PLAYERS.PLAYER_1) {
-      board[y][x] = 'X';
-    } else {
-      board[y][x] = 'O';
+  const handleClick = (index) => {
+    board[index] = turn === PLAYERS.PLAYER_1 ? 'x' : 'o';
+
+    const nextTurn =
+      turn === PLAYERS.PLAYER_1 ? PLAYERS.PLAYER_2 : PLAYERS.PLAYER_1;
+
+    const winner = findWinner();
+
+    if (winner === 'x') setWinner(PLAYERS.PLAYER_1);
+    if (winner === 'o') setWinner(PLAYERS.PLAYER_2);
+
+    if (board.every((square) => square !== null)) {
+      setTie(true);
+    }
+
+    if (winner || tie) {
+      return;
     }
 
     setBoard(board);
-    setTurn(turn === PLAYERS.PLAYER_1 ? PLAYERS.PLAYER_2 : PLAYERS.PLAYER_1);
+    setTurn(nextTurn);
+  };
 
-    console.log(board, turn);
+  const findWinner = () => {
+    const winnerCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let row of winnerCombinations) {
+      const [a, b, c] = row;
+
+      if (board[a] === board[b] && board[b] === board[c]) {
+        return board[a];
+      }
+    }
   };
 
   return (
     <div className="App">
-      <h1>Player {turn}'s turn</h1>
+      {winner ? (
+        <h1>{winner} has won!</h1>
+      ) : tie ? (
+        <h1>It's a tie...</h1>
+      ) : (
+        <h1>{turn}'s turn</h1>
+      )}
 
       <div className="Board">
-        {board.map((row, i) => (
-          <div className="Board__row">
-            {row.map((square, j) => (
-              <div
-                className="Board__row__square"
-                onClick={() => handleClick(i, j)}
-              >
-                {board[i][j]}
-              </div>
-            ))}
-          </div>
+        {board.map((square, i) => (
+          <button
+            className="Board__square"
+            onClick={() => handleClick(i)}
+            disabled={square}
+          >
+            {square}
+          </button>
         ))}
       </div>
     </div>
