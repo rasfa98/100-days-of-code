640e08698f9e6f5fdcee3a9e785fa39b3c5a367a
Rasmus Falk <1998.rasmus.falk@gmail.com>
2021-02-18 19:52:00 +0100

    Password generator updates

--- password-generator/main.go
+++ password-generator/main.go
@@ -2,11 +2,31 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 
 	"github.com/thatisuday/commando"
 )
 
+func generatePassword(length, lowercase, uppercase, symbols, numbers int) string {
+	var random = rand.Seed(time.Now().UnixNano())
+	// var allowedLowercaseCharacters = "abcdedfghijklmnopqrst"
+	// var allowedUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	var allowedSymbols = "!@#$%&*"
+	// var allowedNumbers = "0123456789"
+	// var all = lowercase + uppercase + symbols + numbers
+
+	var password = ""
+
+	for i := 0; i < symbols; i++ {
+		password += string(allowedSymbols[random.Intn(len(allowedSymbols))])
+	}
+
+	return password
+}
+
 func main() {
+
 	commando.
 		SetExecutableName("pw").
 		SetVersion("1.0.0").
@@ -14,30 +34,16 @@ func main() {
 
 	commando.
 		Register(nil).
-		AddFlag("length,len", "password length", commando.Int, 12).
-		AddFlag("uppercase,up", "should password contain uppercase letters", commando.Bool, nil).
-		AddFlag("lowercase,low", "should password contain lowercase letters", commando.Bool, nil).
-		AddFlag("numbers,num", "should password contain numbers", commando.Bool, nil).
-		AddFlag("symbols,sym", "should password contain symbols", commando.Bool, nil).
+		AddFlag("length", "password length", commando.Int, 12).
+		AddFlag("uppercase", "should password contain uppercase letters", commando.Bool, 3).
+		AddFlag("lowercase", "should password contain lowercase letters", commando.Bool, 5).
+		AddFlag("numbers", "should password contain numbers", commando.Bool, 0).
+		AddFlag("symbols", "should password contain symbols", commando.Bool, 4).
 		SetAction(func(args map[string]commando.ArgValue, flags map[string]commando.FlagValue) {
 			for k, v := range flags {
 				fmt.Printf("flag -> %v: %v(%T)\n", k, v.Value, v.Value)
 			}
-		})
 
-	commando.
-		Register("info").
-		SetShortDescription("generates secure passwords.").
-		SetDescription("Generates secure password by allowing the user to specify different password criterias.").
-		AddFlag("length,len", "password length", commando.Int, 12).
-		AddFlag("uppercase,up", "should password contain uppercase letters", commando.Bool, nil).
-		AddFlag("lowercase,low", "should password contain lowercase letters", commando.Bool, nil).
-		AddFlag("numbers,num", "should password contain numbers", commando.Bool, nil).
-		AddFlag("symbols,sym", "should password contain symbols", commando.Bool, nil).
-		SetAction(func(args map[string]commando.ArgValue, flags map[string]commando.FlagValue) {
-			for k, v := range flags {
-				fmt.Printf("flag -> %v: %v(%T)\n", k, v.Value, v.Value)
-			}
 		})
 
 	commando.Parse(nil)
